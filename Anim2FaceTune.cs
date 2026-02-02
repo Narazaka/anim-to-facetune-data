c2989a8a87410058902a7c3ae07cb81d51f539d1
Narazaka <info@narazaka.net>
2026-02-02 18:36:41 +0900

    IEditorOnly

--- Anim2FaceTune.cs
+++ Anim2FaceTune.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
+using VRC.SDKBase;
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Narazaka.VRChat.Anim2FaceTune.Editor")]
 
 namespace Narazaka.VRChat.Anim2FaceTune
 {
-    public class Anim2FaceTune : MonoBehaviour
+    public class Anim2FaceTune : MonoBehaviour, IEditorOnly
     {
         [SerializeField] internal AnimationClip[] clips = new AnimationClip[0];
     }
