644fd5398d6c02234b20dd6c6ffb01ab7e7b8020
Narazaka <info@narazaka.net>
2026-08-29 19:18:49 +0900

    beta.16対応

--- Editor/Anim2FaceTuneEditor.cs
+++ Editor/Anim2FaceTuneEditor.cs
@@ -1,7 +1,8 @@
-using Aoyon.FaceTune;
+using System;
 using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Narazaka.VRChat.Anim2FaceTune.Editor
 {
@@ -17,6 +18,7 @@ namespace Narazaka.VRChat.Anim2FaceTune.Editor
 
         public override void OnInspectorGUI()
         {
+            EnsureTypes();
             EditorGUILayout.HelpBox("This component converts AnimationClips to FaceTune ExpressionDataComponents as child GameObjects when clips are assigned.", MessageType.Info);
             serializedObject.UpdateIfRequiredOrScript();
             EditorGUILayout.PropertyField(clips, true);
@@ -30,8 +32,16 @@ namespace Narazaka.VRChat.Anim2FaceTune.Editor
                     var clip = clipProp.objectReferenceValue as AnimationClip;
                     if (clip == null) continue;
                     var go = new GameObject(clip.name);
-                    var expressionData = go.AddComponent<ExpressionDataComponent>();
-                    expressionData.Clip = clip;
+                    
+                    var expressionData = go.AddComponent(ExpressionDataComponent);
+                    var facialBlendShapes = FacialBlendShapes.GetValue(expressionData);
+                    if (facialBlendShapes == null)
+                    {
+                        facialBlendShapes = Activator.CreateInstance(FacialBlendShapeData);
+                        FacialBlendShapes.SetValue(expressionData, facialBlendShapes);
+                    }
+                    Clip.SetValue(facialBlendShapes, clip);
+                    
                     go.transform.SetParent(transform, false);
                     created.Add(go);
                 }
@@ -39,6 +49,20 @@ namespace Narazaka.VRChat.Anim2FaceTune.Editor
                 EditorGUIUtility.PingObject(created[0]);
             }
             serializedObject.ApplyModifiedProperties();
+        }
+
+        static Type ExpressionDataComponent;
+        static Type FacialBlendShapeData;
+        static FieldInfo FacialBlendShapes;
+        static FieldInfo Clip;
+
+        public static void EnsureTypes()
+        {
+            if (ExpressionDataComponent != null) return;
+            ExpressionDataComponent = Type.GetType("Aoyon.FaceTune.ExpressionDataComponent, Aoyon.FaceTune.Runtime");
+            FacialBlendShapeData = Type.GetType("Aoyon.FaceTune.FacialBlendShapeData, Aoyon.FaceTune.Runtime");
+            FacialBlendShapes = ExpressionDataComponent.GetField("FacialBlendShapes", BindingFlags.Instance | BindingFlags.Public);
+            Clip = FacialBlendShapeData.GetField("Clip", BindingFlags.Instance | BindingFlags.Public);
         }
     }
 }
